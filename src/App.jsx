d1f94b596c22c82edd6ccc7c84218fbabf693b0c
MdAlaminAhmed <contact.alamin02@gmail.com>
2025-11-05 18:25:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Game
+//   -> Board
+//       -> Square
+//   -> History
+
+export default function App() {
+  return (
+    <>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9">
+        {" "}
+        X
+      </button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9">
+        {" "}
+        X
+      </button>
+    </>
+  );
+}
